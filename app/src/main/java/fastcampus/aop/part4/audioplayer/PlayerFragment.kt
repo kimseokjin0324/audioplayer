3aefff295f2552b2ede5c3741eac332ed290d7d5
kimseokjin0324 <kimseokjin0324@gmail.com>
2023-05-15 22:04:49 +0900

    AudioPlayer UI구성 하기 (Player부분은 Fragment로 구성)

---
+++ app/src/main/java/fastcampus/aop/part4/audioplayer/PlayerFragment.kt
@@ -0,0 +1,18 @@
+package fastcampus.aop.part4.audioplayer
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class PlayerFragment : Fragment(R.layout.fragment_player) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+
+        fun newInstance(): PlayerFragment {
+            return PlayerFragment()
+        }
+    }
+}
